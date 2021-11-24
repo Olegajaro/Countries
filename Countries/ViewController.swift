c4b63d6d63a06b6eac1091ffdb481a69f895445d
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-11-24 23:07:42 +0300

    Created tableView and customTableViewCell

--- Countries/ViewController.swift
+++ Countries/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let countryList = ["USA", "UK", "Italy",
+                       "Russia", "Spain", "Canada",
+                       "Australia", "Finland"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(
+            UINib(nibName: "CountriesListTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "countryCell"
+        )
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        
+        countryList.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "countryCell",
+            for: indexPath
+        ) as! CountriesListTableViewCell
+        
+        cell.mainLabel.text = countryList[indexPath.row]
+        
+        return cell
+        
+    }
+}
---
+++ Countries/Views/CountriesListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CountriesListTableViewCell.swift
+//  Countries
+//
+//  Created by Олег Федоров on 24.11.2021.
+//
+
+import UIKit
+
+class CountriesListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
